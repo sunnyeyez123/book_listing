3eb15b079f90dc65b641975279432b9e056f50ca
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-01-04 20:01:39 -0800

    create book class

--- app/src/main/java/com/jasminelawrence/booklisting/Book.java
+++ app/src/main/java/com/jasminelawrence/booklisting/Book.java
@@ -6,7 +6,45 @@ package com.jasminelawrence.booklisting;
 
 public class Book {
 
-    public Book(){
+    private String mTitle, mAuthor, mPublished, mPrice;
+    private double mPageCount;
 
+    public Book(String title, String author, String published, String price, double pageCount){
+        mTitle = title;
+        mAuthor = author;
+        mPublished =published;
+        mPrice = price;
+        mPageCount=pageCount;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getPublished() {
+        return mPublished;
+    }
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public double getPageCount() {
+        return mPageCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mPublished='" + mPublished + '\'' +
+                ", mPrice='" + mPrice + '\'' +
+                ", mPageCount=" + mPageCount +
+                '}';
     }
 }
