9a19df4689413f0a1fe40137c44e5f0ff425cc07
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-01-04 19:27:52 -0800

    display text submitted and add string resources

--- app/src/main/java/com/jasminelawrence/booklisting/MainActivity.java
+++ app/src/main/java/com/jasminelawrence/booklisting/MainActivity.java
@@ -1,13 +1,43 @@
 package com.jasminelawrence.booklisting;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView searchBoxTextView,results ;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        searchBoxTextView = (TextView) findViewById(R.id.search_box);
+        submitButton = (Button) findViewById(R.id.submit_button);
+
+        results = (TextView) findViewById(R.id.submitted_text);
+
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                results.setText(searchBoxTextView.getText());
+            }
+        });
+
+
+
+
+
     }
 }
